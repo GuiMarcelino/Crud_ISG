a3e2aca9094125a4f9b15840e72c555df74f9f45
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2023-09-28 11:25:33 -0300

    refactor(Seed):

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,27 +9,42 @@ user = User.create(
 
 # Verificando se o usuário foi criado com sucesso
 if user.persisted?
+  puts "Usuário criado com sucesso!"
+
   # Criando posts
-  5.times do |post_index|
+  post_success = true
+  5.times do
     post = Post.create(
-      title: "Post #{post_index + 1}",
-      text: "#{FFaker::Lorem.word} #{post_index + 1}",
+      title: "Post",
+      text: "#{FFaker::Lorem.word}",
       user_id: user.id
     )
 
-    # Verificando se o post foi criado com sucesso
-    if post.persisted?
-      # Criando comentários para o post
-      4.times do |comment_index|
-        Comment.create(
-          description: "#{FFaker::Lorem.word} #{comment_index + 1} for post #{post_index + 1}",
-          post_id: post.id
-        )
-      end
-    else
-      puts "Falha ao criar post #{post_index + 1}. Erros: #{post.errors.full_messages.join(', ')}"
+    unless post.persisted?
+      post_success = false
+      puts "Falha ao criar post. Erros: #{post.errors.full_messages.join(', ')}"
     end
   end
+  puts "Posts criados com sucesso!" if post_success
+
+  # Criando comentários para os posts
+  comment_success = true
+  Post.all.each do |post|
+    4.times do
+      comment = Comment.create(
+        name: FFaker::NameBR.name,
+        description: "#{FFaker::Lorem.word}",
+        post_id: post.id
+      )
+
+      unless comment.persisted?
+        comment_success = false
+        puts "Falha ao criar comentário. Erros: #{comment.errors.full_messages.join(', ')}"
+      end
+    end
+  end
+  puts "Comentários criados com sucesso!" if comment_success
+
 else
   puts "Falha ao criar usuário. Erros: #{user.errors.full_messages.join(', ')}"
 end
